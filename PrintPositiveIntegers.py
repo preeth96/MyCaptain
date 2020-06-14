273ad6f60eb09ec597d2b3d1f271d44b400d21bc
preeth96 <65329452+preeth96@users.noreply.github.com>
2020-06-14 11:50:59 +0530

    Update PrintPositiveIntegers.py

--- PrintPositiveIntegers.py
+++ PrintPositiveIntegers.py
@@ -1,6 +1,11 @@
-MyList=[9,-3,-12,8,22,-99]
+L=[]
 J=[]
-for i in MyList:
+N=int(input("Enter Your fav Number"))
+for i in range(N):
+    X=int(input("Enter the elements  to the list"))
+    L.append(X)
+print(L)
+for i in L:
     if i>0:
         J.append(i)
 print(J)
